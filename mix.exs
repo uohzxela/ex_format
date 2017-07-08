ca3a14a747cded4bdd67246d8be58d91510f4947
uohzxela <uohzxela@gmail.com>
2017-07-08 19:13:05 +0800

    Remove CLI and other redundant files

--- mix.exs
+++ mix.exs
@@ -2,11 +2,9 @@ defmodule Formatter.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :exfmt,
+    [app: :ex_format,
      version: "0.1.0",
      elixir: "~> 1.4",
-     escript: [main_module: ExFormat.CLI],
-     build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps()]
   end
