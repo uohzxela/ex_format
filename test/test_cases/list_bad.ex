344c7332a12281257db4b38be89fcdff214cfc8a
uohzxela <uohzxela@gmail.com>
2017-07-12 06:21:29 +0800

    Add tests for comments for list line splitting

--- test/test_cases/list_bad.ex
+++ test/test_cases/list_bad.ex
@@ -1,7 +1,9 @@
 [1,2,3,4,5,3,5,6,7,8]
 
 [1,
+# comment 1
 2,3,
+# comment 2
 4,5,3,5,
 6,7,8]
 
@@ -12,3 +14,11 @@
 [3.4,6,7]]
 
 [1,2,3,"longlonglongstring", "longlonglongstring", "longlonglongstring", 12319910623, 9, "longlonglongstring", "longlonglongstring"]
+
+[:this_is_a_very_long_atom,
+:here_is_the_next_atom, # inline comment1
+# comment1
+:are_we_going_to_see, # inline comment 2
+# comment2
+:any_comments?
+]
