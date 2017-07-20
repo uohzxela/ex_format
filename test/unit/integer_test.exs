5968a26f7e193556ecb2b62abb8f1cec2b00db98
uohzxela <uohzxela@gmail.com>
2017-07-21 03:42:55 +0800

    Remove >>> usage and white spaces, add safety check

--- test/unit/integer_test.exs
+++ test/unit/integer_test.exs
@@ -5,7 +5,7 @@ defmodule ExFormat.Unit.IntegerTest do
   describe "integer literal" do
     test "binary representation" do
       assert_format_string("0b1010\n")
-      "0b0101" >>> "0b101\n"
+      assert_format_string("0b0101", "0b101\n")
     end
 
     test "octal representation" do
@@ -14,12 +14,12 @@ defmodule ExFormat.Unit.IntegerTest do
 
     test "decimal representation" do
       assert_format_string("123\n")
-      "01" >>> "1\n"
+      assert_format_string("01", "1\n")
     end
 
     test "hexadecimal representation" do
       assert_format_string("0xEF\n")
-      "0xef" >>> "0xEF\n"
+      assert_format_string("0xef", "0xEF\n")
     end
 
     test "char representation" do
--- test/unit/kw_list_syntax_test.exs
+++ test/unit/kw_list_syntax_test.exs
@@ -4,67 +4,38 @@ defmodule ExFormat.Unit.KwListSyntaxTest do
 
   describe "preserves keyword list syntax" do
     test "for function definitions" do
-      """
-      def   func2(a2,b2),   do: func3(a,b)
-      """ >>>
-      """
-      def func2(a2, b2), do: func3(a, b)
-      """
-
-      """
-      def func1(a1,b1) do
-          func3(a,b)
-      end
-      """ >>>
-      """
-      def func1(a1, b1) do
-        func3(a, b)
-      end
-      """
+      assert_format_string("def func2(a2, b2), do: func3(a, b)\n")
+      assert_format_string(
+        """
+        def func1(a1, b1) do
+          func3(a, b)
+        end
+        """
+      )
     end
 
     test "for quote" do
-      """
-      quote   do: 1+2
-      """ >>>
-      """
-      quote do: 1 + 2
-      """
-
-      """
-      quote do
-      1+2
-      end
-      """ >>>
-      """
-      quote do
-        1 + 2
-      end
-      """
+      assert_format_string("quote do: 1 + 2\n")
+      assert_format_string(
+        """
+        quote do
+          1 + 2
+        end
+        """
+      )
     end
 
     test "for if else statement" do
-      """
-      if something, do: that_thing, else: nothing
-      """ >>>
-      """
-      if something, do: that_thing, else: nothing
-      """
-
-      """
-      if something do
-      that_thing
-      else
+      assert_format_string("if something, do: that_thing, else: nothing\n")
+      assert_format_string(
+        """
+        if something do
+          that_thing
+        else
           nothing
-      end
-      """ >>>
-      """
-      if something do
-        that_thing
-      else
-        nothing
-      end
-      """
+        end
+        """
+      )
     end
   end
 end
