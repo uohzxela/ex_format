c6a8cc768788c0eedb24ada982cd5612490d31db
uohzxela <uohzxela@gmail.com>
2017-07-21 07:50:06 +0800

    Add tests for white space

--- test/ex_format_test.exs
+++ test/ex_format_test.exs
@@ -39,24 +39,25 @@ defmodule ExFormatTest do
   #   assert_format_file("sigils")
   # end
 
-  test "spaces around binary operators, after commas, colons and semicolons" do
-    assert_format_file("spaces_in_code")
-  end
+  # test "spaces around binary operators, after commas, colons and semicolons" do
+  #   assert_format_file("spaces_in_code")
+  # end
 
-  test "no spaces after unary operators and inside range literals, the only exception is the not operator" do
-    assert_format_file("no_spaces_in_code")
-  end
+  # test "no spaces after unary operators and inside range literals, the only exception is the not operator" do
+  #   assert_format_file("no_spaces_in_code")
+  # end
 
-  test "spaces around default arguments \\ definition" do
-    assert_format_file("default_arguments")
-  end
+  # test "spaces around default arguments \\ definition" do
+  #   assert_format_file("default_arguments")
+  # end
 
-  test "no spaces around segment options defintion in bitstrings" do
-    assert_format_file("bitstring_segment_options")
-  end
+  # test "no spaces around segment options defintion in bitstrings" do
+  #   assert_format_file("bitstring_segment_options")
+  # end
 
+  # TODO
   # test "use parentheses around def arguments, don't omit them even when a function has no arguments" do
-  #   assert_formatted_content("fun_parens")
+  #   assert_format_file("fun_parens")
   # end
 
   test "when using atom literals that need to be quoted, use double quotes around the atom name" do
