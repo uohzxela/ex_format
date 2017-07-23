8384febc34f9bb6a84024544de55f4d21fe54901
uohzxela <uohzxela@gmail.com>
2017-07-24 00:36:03 +0800

    Do not omit parens for 0-arity function definitions

---
+++ test/unit/fun_parens_test.exs
@@ -0,0 +1,56 @@
+defmodule ExFormat.Unit.FunParensTest do
+  import Test.Support.Unit
+  use ExUnit.Case
+
+  describe "always use parentheses around def/defp/defmacro/defmacrop arguments" do
+    test "for functions with arguments" do
+      assert_format_string("def f(a), do: something\n")
+      assert_format_string("defp f(a), do: something\n")
+      assert_format_string("defmacro f(a), do: something\n")
+      assert_format_string("defmacrop f(a), do: something\n")
+    end
+
+    test "for functions with no arguments" do
+      assert_format_string("def f(), do: something\n")
+      assert_format_string("defp f(), do: something\n")
+      assert_format_string("defmacro f(), do: something\n")
+      assert_format_string("defmacrop f(), do: something\n")
+    end
+
+    test "for multiple function definitions in a block" do
+      bad = """
+      def f, do: something
+      defp f, do: something
+      defmacro f, do: something
+      defmacrop f, do: something
+      """
+      good = """
+      def f(), do: something
+      defp f(), do: something
+      defmacro f(), do: something
+      defmacrop f(), do: something
+      """
+      assert_format_string(bad, good)
+
+      bad = """
+      def main arg1, arg2 do
+        something
+      end
+
+      def main do
+        something
+      end
+      """
+      good = """
+      def main(arg1, arg2) do
+        something
+      end
+
+      def main() do
+        something
+      end
+      """
+      assert_format_string(bad, good)
+    end
+  end
+end
