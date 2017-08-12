77204db616e898e974b29c84dcdec106ae858f44
uohzxela <uohzxela@gmail.com>
2017-08-12 20:12:37 +0800

    Multiline call args should be aligned together

--- test/unit/fun_args_test.exs
+++ test/unit/fun_args_test.exs
@@ -2,13 +2,25 @@ defmodule ExFormat.Unit.FunArgsTest do
   import Test.Support.Unit
   use ExUnit.Case
 
-  test "split args if they are too long for function calls" do
-    bad = "Enum.map_join(some_very_long_function_name1(args), some_very_long_function_name2(delimiter), &to_string(&1, fun, state))"
-    good = """
-    Enum.map_join(some_very_long_function_name1(args),
-      some_very_long_function_name2(delimiter),
-      &to_string(&1, fun, state))
-    """
-    assert_format_string(bad, good)
+  describe "split args if they are too long for function calls" do
+    test "with remote calls" do
+      bad = "Enum.map_join(some_very_long_function_name1(args), some_very_long_function_name2(delimiter), &to_string(&1, fun, state))"
+      good = """
+      Enum.map_join(some_very_long_function_name1(args),
+                    some_very_long_function_name2(delimiter),
+                    &to_string(&1, fun, state))
+      """
+      assert_format_string(bad, good)
+    end
+
+    test "with local calls" do
+      bad = "local_call(some_very_long_function_name1(args), some_very_long_function_name2(delimiter), &to_string(&1, fun, state))"
+      good = """
+      local_call(some_very_long_function_name1(args),
+                 some_very_long_function_name2(delimiter),
+                 &to_string(&1, fun, state))
+      """
+      assert_format_string(bad, good)
+    end
   end
 end
