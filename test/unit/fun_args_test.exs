7a6e10c1540b3d3f5e2b23807605f026e4d97449
uohzxela <uohzxela@gmail.com>
2017-08-12 20:23:23 +0800

    Split call args if they have intended line break

--- test/unit/fun_args_test.exs
+++ test/unit/fun_args_test.exs
@@ -2,7 +2,7 @@ defmodule ExFormat.Unit.FunArgsTest do
   import Test.Support.Unit
   use ExUnit.Case
 
-  describe "split args if they are too long for function calls" do
+  describe "split function call args if they are too long" do
     test "with remote calls" do
       bad = "Enum.map_join(some_very_long_function_name1(args), some_very_long_function_name2(delimiter), &to_string(&1, fun, state))"
       good = """
@@ -18,6 +18,34 @@ defmodule ExFormat.Unit.FunArgsTest do
       good = """
       local_call(some_very_long_function_name1(args),
                  some_very_long_function_name2(delimiter),
+                 &to_string(&1, fun, state))
+      """
+      assert_format_string(bad, good)
+    end
+  end
+
+  describe "split function call args if they have intended line break" do
+    test "with remote calls" do
+      bad = """
+      Enum.map_join(args,
+        delimiter, &to_string(&1, fun, state))
+      """
+      good = """
+      Enum.map_join(args,
+                    delimiter,
+                    &to_string(&1, fun, state))
+      """
+      assert_format_string(bad, good)
+    end
+
+    test "with local calls" do
+      bad = """
+      local_call(args, delimiter,
+        &to_string(&1, fun, state))
+      """
+      good = """
+      local_call(args,
+                 delimiter,
                  &to_string(&1, fun, state))
       """
       assert_format_string(bad, good)
