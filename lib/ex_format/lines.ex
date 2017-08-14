d97ccd6bcc10e4d629e8a136d0becf94d06cd024
uohzxela <uohzxela@gmail.com>
2017-08-14 22:27:48 +0800

    Initialize agent properly for lines storage

--- lib/ex_format/lines.ex
+++ lib/ex_format/lines.ex
@@ -1,14 +1,10 @@
 defmodule ExFormat.Lines do
   def initialize_lines_store(code_string) do
-    lines = String.split(code_string, "\n")
-    Agent.start_link(fn -> %{} end, name: :lines)
-    for {line, i} <- Enum.with_index(lines) do
-      update_line(i + 1, String.trim(line))
-    end
-   # start_link(code_string)
+   start_link(code_string)
   end
 
   def start_link(code_string) do
+    Agent.start_link(fn -> %{} end, name: :lines)
     lines_map =
       code_string
       |> String.split("\n")
@@ -17,13 +13,11 @@ defmodule ExFormat.Lines do
         {i + 1, String.trim(line)}
       end)
       |> Map.new
-    IO.inspect lines_map
-    Agent.start_link(fn -> lines_map end, name: :lines)
+
+    Agent.update(:lines, &Map.merge(&1, lines_map))
   end
 
-  def get_line(k), do: Agent.get(:lines, fn map -> Map.get(map, k) end)
+  def get_line(k), do: Agent.get(:lines, &Map.get(&1, k))
 
-  def update_line(k, v), do: Agent.update(:lines, fn map -> Map.put(map, k, v) end)
-
-  def clear_line(k), do: Agent.update(:lines, fn map -> Map.put(map, k, nil) end)
+  def clear_line(k), do: Agent.update(:lines, &Map.put(&1, k, nil))
 end
