99bc079942de330593ff0f2d08bd97e646689fc8
uohzxela <uohzxela@gmail.com>
2017-08-15 18:20:00 +0800

    Remove bang version, update ex_doc

--- test/support/unit.ex
+++ test/support/unit.ex
@@ -2,8 +2,8 @@ defmodule Test.Support.Unit do
   import ExUnit.Assertions
 
   def assert_format_string(bad, good) do
-    assert ExFormat.format!(bad) == good
-    assert ExFormat.format!(good) == good
+    assert ExFormat.format(bad) == good
+    assert ExFormat.format(good) == good
   end
 
   def assert_format_string(string) do
