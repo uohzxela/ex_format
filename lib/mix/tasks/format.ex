8cb6b3494d94e2d277c63745571453c34dea13be
uohzxela <uohzxela@gmail.com>
2017-08-16 20:30:19 +0800

    Add mix task to format files

---
+++ lib/mix/tasks/format.ex
@@ -0,0 +1,20 @@
+defmodule Mix.Tasks.Format do
+  use Mix.Task
+
+  alias ExFormat
+
+  @shortdoc "Formats the given code string"
+
+  def run(file_paths) do
+    Enum.map(file_paths, fn file_path ->
+      formatted =
+        file_path
+        |> File.read!()
+        |> ExFormat.format()
+      file = File.open!(file_path, [:write])
+      IO.binwrite(file, formatted)
+      File.close(file)
+      IO.puts("Formatted #{file_path}")
+    end)
+  end
+end
