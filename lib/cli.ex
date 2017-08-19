510c423d9484bb94efde0ea690aaea9221c2530b
uohzxela <uohzxela@gmail.com>
2017-08-19 22:24:46 +0800

    Add escript

---
+++ lib/cli.ex
@@ -0,0 +1,39 @@
+defmodule ExFormat.CLI do
+  @moduledoc """
+  Example usage: ./ex_format lib/**/*.ex config/**/*.exs
+  """
+
+  def main(argv) do
+    argv
+    |> parse_args()
+    |> run()
+  end
+
+  def parse_args(argv) do
+    parse = OptionParser.parse(argv, switches: [help: :boolean], aliases: [h: :help])
+
+    case parse do
+      {[help: true], _, _} ->
+        :help
+      {_, argv, _} ->
+        argv
+    end
+  end
+
+  @doc """
+  Displays a help message.
+  """
+  def run(:help) do
+    IO.puts(@moduledoc)
+  end
+
+  @doc """
+  Formats a list of files.
+  """
+  def run(file_paths) do
+    Enum.each(file_paths, fn file_path ->
+      Mix.Tasks.Format.format_file(file_path)
+      IO.puts("Formatted #{file_path}")
+    end)
+  end
+end
--- lib/mix/tasks/format.ex
+++ lib/mix/tasks/format.ex
@@ -5,12 +5,16 @@ defmodule Mix.Tasks.Format do
 
   def run(file_paths) do
     Enum.each(file_paths, fn file_path ->
-      formatted =
-        file_path
-        |> File.read!()
-        |> ExFormat.format()
-      File.write!(file_path, formatted)
+      format_file(file_path)
       Mix.shell().info("Formatted #{file_path}")
     end)
+  end
+
+  def format_file(file_path) do
+    formatted =
+      file_path
+      |> File.read!()
+      |> ExFormat.format()
+    File.write!(file_path, formatted)
   end
 end
